cf7f9ac51bdab80f42a60bd71435dee99449aa8c
Hayden Jones <haychjones@gmail.com>
2021-08-03 20:21:43 +1000

    events tile added

---
+++ client/src/components/eventTile.js
@@ -0,0 +1,7 @@
+export default function EventTile() {
+  return (
+    <div>
+      <h1> Events Tiles</h1>
+    </div>
+  );
+}
--- client/src/components/yourEvents.js
+++ client/src/components/yourEvents.js
@@ -1,7 +1,10 @@
+import EventTile from "./eventTile";
+
 export default function YourEvents() {
   return (
     <div>
       <h1>YourEventsRoute</h1>
+      <EventTile />
     </div>
   );
 }
