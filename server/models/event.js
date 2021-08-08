162482fcc27669826b02ab4dd39147e8e249fd5e
Hayden Jones <haychjones@gmail.com>
2021-08-08 15:49:06 +1000

    models/events.js added

---
+++ server/models/event.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+  },
+  theme: {
+    type: String,
+    required: true,
+  },
+  host: {
+    type: String,
+  },
+  guests: [
+    {
+      userName: {
+        type: String,
+      },
+    },
+  ],
+  isPrivate: {
+    type: Boolean,
+  },
+});
+
+const Event = model("Event", eventSchema);
+
+module.exports = Event;
