59f7a38f2aab1a0192789d759add666215c1b393
Hayden Jones <haychjones@gmail.com>
2021-08-08 15:53:21 +1000

    typedefs and index added

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,75 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    userName: String!
+    email: String!
+    password: String
+    hosting: [Event]
+    invited: [Event]
+  }
+
+  type Event {
+    _id: ID!
+    title: String!
+    description: String
+    theme: String
+    host: User
+    invitedGuests: [User]
+    goingGuests: [User]
+    isPrivate: Boolean
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(userName: String!): User
+    allUsers: [User]
+    allEvents: [Event]
+    event(id: ID): Event
+  }
+
+  type Mutation {
+    addUser(userName: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    editUser(userName: String, email: String, password: String): Auth
+    addEvent(title: String, description: String, theme: String): User
+    editEvent(
+      eventId: ID
+      title: String
+      description: String
+      theme: String
+      isPrivate: Boolean
+    ): eventUpdate!
+    cancelEvent(eventId: ID): eventUpdate!
+    addGuest(eventId: ID, guestId: String): Event
+  }
+
+  type eventUpdate {
+    success: Boolean!
+    message: String
+    events: [Event]
+  }
+`;
+
+module.exports = typeDefs;
+
+// type Host {
+//     _id: ID!
+//     hostID: User
+//     eventID: [Event]
+// }
+
+// type Guest {
+//     _id: ID!
+//     guestID: [User]
+//     eventID: INT
+//
+// ******* Mutations *******
+// host: Host
+// guest: [Guest]
+// }
