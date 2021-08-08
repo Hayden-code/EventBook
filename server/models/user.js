ab61d6987969e7aad448dede48078f8af2335f18
Hayden Jones <haychjones@gmail.com>
2021-08-08 15:51:08 +1000

    models/user added and models/index added for exporting models

---
+++ server/models/user.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Please enter an email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  hosting: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+  invited: [{ type: Schema.Types.ObjectId, ref: "Event" }],
+});
+
+userSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+  next();
+});
+
+userSchema.methods.checkPassword = async function (password) {
+  return bcrypt.compare(password, this.checkPassword);
+};
+
+const User = model("User", userSchema);
+
+module.exports = User;
